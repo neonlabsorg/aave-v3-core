043e85ccdf1e0f1c7239026b70ecb2aad5e5eb1a
Ivan Adamov <psnasti@gmail.com>
2025-06-18 10:20:02 -0700

    set up CI

--- test-suites/__setup.spec.ts
+++ test-suites/__setup.spec.ts
@@ -1,6 +1,23 @@
 import rawBRE from 'hardhat';
 import { initializeMakeSuite } from './helpers/make-suite';
-import { ethers } from 'hardhat';
+import { ethers, network } from 'hardhat';
+import { TransactionResponse, TransactionReceipt } from '@ethersproject/abstract-provider/src.ts';
+
+async function waitTransactionWithTimeout(
+  tx: TransactionResponse,
+  timeoutMs: number = 120000
+): Promise<TransactionReceipt> {
+  const receipt: TransactionReceipt = (await Promise.race([
+    tx.wait(),
+    new Promise((_, reject) =>
+      setTimeout(
+        () => reject(new Error(`Transaction wait timed out after ${timeoutMs} ms`)),
+        timeoutMs
+      )
+    ),
+  ])) as TransactionReceipt;
+  return receipt;
+}
 
 // Patch Signer.sendTransaction to always wait for the tx
 const patchSigners = () => {
@@ -11,10 +28,9 @@ const patchSigners = () => {
       const originalSend = s.sendTransaction.bind(s);
       s.sendTransaction = async (...args) => {
         const tx = await originalSend(...args);
-        await tx.wait();
+        await waitTransactionWithTimeout(tx);
         return tx;
       };
-      console.log(`Patched signer ${s.address}`);
     }
     return signers;
   };
